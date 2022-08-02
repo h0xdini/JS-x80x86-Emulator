f3b2c7c80862d53cd67c5ae7c44efdf0a1564bac
ALBATARx64 <yohanopp@gmail.com>
2022-08-03 01:33:23 +0200

    mov

--- mov.js
+++ mov.js
@@ -1,8 +1,20 @@
 let generalPurposeRegisters = {
-  ax: 0,
-  bx: 0,
-  cx: 0,
-  dx: 0,
+  ax: {
+  	ah: 0,
+	al: 0,
+  },
+  bx: {
+  	bh: 0,
+	bl: 0,
+  },
+  cx: {
+  	ch: 0,
+	cl: 0,
+  },
+  dx: {
+  	dh: 0,
+	dl: 0,
+  },
   bp: 0,
   sp: 0,
   si: 0,
@@ -11,17 +23,87 @@ let generalPurposeRegisters = {
 
 let memory = []
 
-const movInst = (source, destination) => {
+const movInst = (source, destination, hexTwice) => {
   if (destination.toLowerCase() === 'ax') {
-    generalPurposeRegisters.ax = source
+    if ((source <= 0xFFFF) || (Array.isArray(source))) {
+        if (!hexTwice) {
+          if (source > 255) {
+            generalPurposeRegisters.ax.al = 255
+            generalPurposeRegisters.ax.ah = source - 255
+          } else {
+            generalPurposeRegisters.ax.al = source
+          }
+        } else {
+          if ((source[0] <= 255) && (source[1] <= 255)) {
+            generalPurposeRegisters.ax.al = source[1]
+            generalPurposeRegisters.ax.ah = source[0]
+          } else {
+            alert("Value out of bounds")
+          }
+        }
+    } else {
+      alert("Value out of bounds")
+    }
   } else if (destination.toLowerCase() === 'bx') {
-    generalPurposeRegisters.bx = source
-  } else if (destination.toLowerCase() === 'bx') {
-    generalPurposeRegisters.bx = source
+    if ((source <= 0xFFFF) || (Array.isArray(source))) {
+        if (!hexTwice) {
+          if (source > 255) {
+            generalPurposeRegisters.bx.bl = 255
+            generalPurposeRegisters.bx.bh = source - 255
+          } else {
+            generalPurposeRegisters.bx.bl = source
+          }
+        } else {
+          if ((source[0] <= 255) && (source[1] <= 255)) {
+            generalPurposeRegisters.bx.bl = source[1]
+            generalPurposeRegisters.bx.bh = source[0]
+          } else {
+            alert("Value out of bounds")
+          }
+        }
+    } else {
+      alert("Value out of bounds")
+    }
   } else if (destination.toLowerCase() === 'cx') {
-    generalPurposeRegisters.cx = source
+    if ((source <= 0xFFFF) || (Array.isArray(source))) {
+        if (!hexTwice) {
+          if (source > 255) {
+            generalPurposeRegisters.cx.cl = 255
+            generalPurposeRegisters.cx.ch = source - 255
+          } else {
+            generalPurposeRegisters.cx.cl = source
+          }
+        } else {
+          if ((source[0] <= 255) && (source[1] <= 255)) {
+            generalPurposeRegisters.cx.cl = source[1]
+            generalPurposeRegisters.cx.ch = source[0]
+          } else {
+            alert("Value out of bounds")
+          }
+        }
+    } else {
+      alert("Value out of bounds")
+    }
   } else if (destination.toLowerCase() === 'dx') {
-    generalPurposeRegisters.dx = source
+    if ((source <= 0xFFFF) || (Array.isArray(source))) {
+        if (!hexTwice) {
+          if (source > 255) {
+            generalPurposeRegisters.dx.dl = 255
+            generalPurposeRegisters.dx.dh = source - 255
+          } else {
+            generalPurposeRegisters.dx.dl = source
+          }
+        } else {
+          if ((source[0] <= 255) && (source[1] <= 255)) {
+            generalPurposeRegisters.dx.dl = source[1]
+            generalPurposeRegisters.dx.dh = source[0]
+          } else {
+            alert("Value out of bounds")
+          }
+        }
+    } else {
+      alert("Value out of bounds")
+    }
   } else if (destination.toLowerCase() === 'si') {
     generalPurposeRegisters.si = source
   } else if (destination.toLowerCase() === 'di') {
@@ -32,24 +114,31 @@ const movInst = (source, destination) => {
     generalPurposeRegisters.bp = source
   } else if (/([0-9][0-9][0-9][0-9][0-9])/.test(destination)) {
     const memoryPosition = Number(destination.slice(1, destination.length - 1))
-    console.log(Number(destination.slice(1, destination.length - 1)))
     memory[memoryPosition] = source
   }
 }
 
 // Intrepreter
 const instructionThriceSplit = (movInstruction) => {
+  let twice = false
   if (movInstruction.startsWith('mov')) {
     let segs = movInstruction.split(' ')
     const char = '"'
     if ((segs[2].includes(char)) && (segs[2].length === 3)) {
-    	segs[2] = convertASCIItoHex((JSON.parse(segs[2])))	  
-    } if ((segs[2].includes(char)) && (segs[2].length > 3)) {
+    	segs[2] = Number(`0x${convertASCIItoHex((JSON.parse(segs[2])))}`)
+    } else if ((segs[2].includes(char)) && (segs[2].length === 4)) {
+      
+    	segs[2] = [
+          Number(`0x${convertASCIItoHex((segs[2][1]))}`),
+          Number(`0x${convertASCIItoHex((segs[2][2]))}`)
+        ]	
+        twice = true
+    } else if ((segs[2].includes(char)) && (segs[2].length > 3)) {
     	segs[2] = (JSON.parse(segs[2]))  
     } else {
       segs[2] = Number((JSON.parse(segs[2])))	  
     }
-    movInst(segs[2], segs[1].slice(0, segs[1].length-1))
+    movInst(segs[2], segs[1].slice(0, segs[1].length-1), twice)
   }
 }
 
@@ -60,5 +149,5 @@ function convertASCIItoHex(asciiVal) {
 }
 
 // tests
-instructionThriceSplit('mov [00233], "2"')
-console.log(memory[233])
+instructionThriceSplit('mov ax, "AB"')
+console.log(generalPurposeRegisters.ax)
